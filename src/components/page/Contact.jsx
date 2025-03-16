6f9318e4e6d1ce412ea060c754a1c1a57d520c3b
FannyRz <fanny.rz.celia@gmail.com>
2025-03-16 15:52:39 +0100

    mise en page du form

--- src/components/page/Contact.jsx
+++ src/components/page/Contact.jsx
@@ -1,13 +1,24 @@
-import './Accueil.css';
+import './Contact.css';
 import Header from '../Header.jsx';
 import Footer from '../Footer.jsx';
+import ContactForm from '../ContactForm.jsx';
 
 
 export default function Contact() {
     return (
             <div>
               <Header />
-              <h1>Welcome to Contact page</h1>
+              <div id="contact">
+              <h1>Nous contacter</h1>
+              <p cl> Vous avez des questions concernant nos livres Disney, des suggestions ou des commentaires ? 
+                <br/> N'hésitez pas à nous contacter ! Nous serons ravis de vous répondre. </p>
+              </div>
+              <ContactForm />
+              <div id="contact">
+                <h3>Informations de contact supplémentaires :</h3>
+                  <p>Email : contact@disneylibrarie.com</p>
+                  <p>Téléphone : +33 7 23 45 67 89</p>
+              </div>
               <Footer />
             </div>
           )
